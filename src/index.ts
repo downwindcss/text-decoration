a737678875e799f216cbbdf53a4cfa7610ce0af4
Sung M. Kim <sungmkim80@gmail.com>
2021-04-15 15:24:03 -0400

    Removed old text-decoration-style.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-// import textDocorationStyle from './plugins/text-decoration-style';
-// module.exports = {
-//   textDocorationStyle,
-// };
-
 import textDecoration from './textDecoration';
 
 module.exports = textDecoration;
